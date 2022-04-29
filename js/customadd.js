22fab3e9c7049278e9852540deb7ebde559b12be
Isaac Ojo <hojoisaac@gmail.com>
2022-04-29 12:52:23 +0100

    tweaks

--- js/customadd.js
+++ js/customadd.js
@@ -1,3 +1,5 @@
+var Web3 = import ('/js/web3.min.js');
+
 function myFunction() {
     alert("IDO Coming Soon");
 }
@@ -26,13 +28,13 @@ function opentab(evt, tabName) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 document.getElementById("walletbutton").onclick = function () {
-    connectPlaceHolder()
+    connectWallet()
 };
 document.getElementById("claim-airdrops").onclick = function () {
     claimPlaceHolder()
 };
-document.getElementById("buy-button").onclick = function () {
-    claimPlaceHolder()
+document.getElementById("connect-buy").onclick = function () {
+    connectWallet()
 };
 
 function claimPlaceHolder() {
@@ -41,4 +43,48 @@ function claimPlaceHolder() {
 
 function connectPlaceHolder() {
     alert("Still working on it");
+}
+
+async function connectWallet() {
+    if (window.ethereum) {
+        var web3 = new Web3(window.ethereum);
+        await window.ethereum.enable()
+        const networkId = await web3.eth.net.getId();
+        if (networkId != 97) {
+            window.alert("Wrong network detected. Please switch to the BNB smart chain network.");
+            //window.Error("Wrong network detected. Please switch to the BNB smart chain network.");
+        } else {
+           
+            document.getElementById("buy-button").style.display = "inline-block";
+            document.getElementById("claim-tokens").style.display = "inline-block";
+            document.getElementById("connect-buy").style.display = "none";
+            window.alert("Connected to the BNB smart chain network.");
+            await window.ethereum.request({method:'eth_requestAccounts'});
+            var accounts = await web3.eth.getAccounts();
+            console.log(accounts)
+            var account = accounts[0];
+            //make account text show only begiining and end of address
+            var accountText = account.substring(0, 6) + "..." + account.substring(account.length - 4, account.length);
+            document.getElementById('walletbutton').textContent = accountText;
+            balance = await web3.eth.getBalance(account)
+            //get ETH Balance IN 2
+            document.getElementById('bal').textContent = await web3.utils.fromWei(balance, "ether") + " BNB";
+
+           
+            
+            
+
+          
+           
+
+            
+        }
+        
+
+        //contract = new web3.eth.Contract(ABI, ADDRESS);
+    } else {
+          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    
+    return web3
 }
